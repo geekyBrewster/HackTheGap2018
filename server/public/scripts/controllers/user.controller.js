06fbe1d8ab616dc0f3c3353159d76d625f9e85c0
Amanda Kirchner <geeky.brewster@gmail.com>
2018-01-07 09:07:28 -0600

    Removed extra html headers

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -3,4 +3,9 @@ myApp.controller('UserController', function(UserService) {
   var vm = this;
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
+
+  //vm.allPilltakers = UserService.allPilltakers;
+  //vm.userObject.getAllPilltakers();
+
+  vm.patients = [{firstName: 'Bob', lastName:'Johnson'}, {firstName: 'Mary', lastName: 'Peterson'}];
 });
--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -2,8 +2,25 @@ myApp.factory('UserService', function($http, $location){
   console.log('UserService Loaded');
 
   var userObject = {};
+  var allPilltakers = [];
+  var caretakerID = 1;
+
+  // GET single pilltaker for a caretaker
+/*  function getAllPilltaker(caretakerID){
+    console.log('in getAllPilltakers');
+
+    $http.get('/all/' + caretakerID).then(function(response) {
+      console.log(response.data);
+      allPilltakers = response.data;
+      console.log('single pilltaker is:', allPilltakers);
+    });
+  }
+
+  getAllPilltakers(1);*/
 
   return {
+
+    allPilltakers : allPilltakers,
     userObject : userObject,
 
     getuser : function(){
